6e15b32aeeaee51b85a2aa11bb06f5e117bd4ce0
miguelrs77 <miguelr.s77@gmail.com>
2023-08-18 20:01:34 -0300

    creamos las carpetas

---
+++ Javascript/semana1.js
@@ -0,0 +1,25 @@
+/*
+Clase 1:Clases Parte 1
+Laboratorio IV: JAVASCRIPT
+Team: The Informatic Hamsters
+Scrum Master: Quiquinto Romina Ayelen (abandono la carrera 29-05-2023)
+Scrum Master: Giuliana Paola Diaz Luna 
+*/
+
+
+// 6.3 hoisting y clases: Parte 1 y 2 - Alumno: Giuliana Paola Diaz Luna
+// Hosting: no se puede crear una objeto antes de haber definido la clase
+// let persona1 = new Persona('Martín', 'Pérez'); 
+
+
+// 6.1 Sintaxis de Clases en JavaScript: Parte 1 y 2 - Alumno: Giuliana Paola Diaz Luna
+// Se recomienda que el nombre de la clase empiece con mayúscula para poder reconocer fácilmente las clases en JavaScript. 
+// Una clase puede tener atributos y métodos. Si no se define un constructor JavaScript agregará uno vacío de manera automática.
+
+
+    // 6.2 Método Get y Set: Parte Get y Parte Set - Alumno: Nadia Acosta
+    
+  
+   
+
+// 6.4 Herencia: Parte 1 y 2 - Alumno: Giuliana Paola Diaz Luna
