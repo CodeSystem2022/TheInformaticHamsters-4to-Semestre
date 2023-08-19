9a2f380aa5d8704b9b72b765a0f43176894a7436
Marcelo Alejandro Quispe <112019566+MarceloAlejandroQuispe@users.noreply.github.com>
2023-08-18 22:00:15 -0300

    Add files via upload

---
+++ PYTHON/PythonSemana1.py
@@ -0,0 +1,32 @@
+
+#Clase 1: Clases Parte 1
+#Laboratorio IV: PYTHON
+#Team: The Informatic Hamsters
+#Scrum Master: Quispe Marcelo Alejandro
+
+
+
+
+
+# 1.1 Comenzamos con el laboratorio de usuario: como tarea se debe crear el UML - Alumno:  
+
+
+# 1.2 Comenzamos a cargar la base de datos -> continuamos con postgresql - Alumno: 
+
+
+# 1.3 Vamos a avanzar con la clase entidad Usuario  - Alumno : 
+    
+  
+   
+# 1.4 Comenzamos con la Clase de UsuarioDAO - Alumno: 
+    
+    
+# 1.5 Comenzamos las pruebas de la Clase UsuarioDAO y menu_app_usuario - Alumno:
+    
+
+
+# 1.6 Comenzamos a generar las diferentes opciones en menu_app_usuario - Alumno:
+
+
+
+# 1.7 Prueba final - Alumno :
