544442350b4f783807193ebe1f4d45e404a27a22
nadiaacosta <nadia.8acosta@gmail.com>
2023-08-26 21:33:08 +0200

    Seamana02-JAVA

---
+++ JAVA/Leccion01/estudiantes/src/test/java/unt/estudiantes/EstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package unt.estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
