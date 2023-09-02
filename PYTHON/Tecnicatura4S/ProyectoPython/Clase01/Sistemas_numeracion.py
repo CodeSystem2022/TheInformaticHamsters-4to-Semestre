5c24b953b8bbf234e583e8250e4c30ca3dda3c72
miguelrs77 <miguelr.s77@gmail.com>
2023-09-01 21:43:15 -0300

    mi asistencia python

--- PYTHON/Tecnicatura4S/ProyectoPython/Clase01/Sistemas_numeracion.py
+++ PYTHON/Tecnicatura4S/ProyectoPython/Clase01/Sistemas_numeracion.py
@@ -16,3 +16,29 @@ print (f'a octal:{a}')
 # Sistema hexadecimal
 a = 0xA
 print (f'a hexadecimal:{a}')
+
+# 2.2 Seguimos con 
+# -> Decimal, Binario, Octal y Hexadecimal
+#  Ahora con int('valor', base) Alumno : Miguel Rodriguez Saquilan
+
+
+
+# Base decimal
+a = int('23', 10)
+print(f'a = 10 Base decimal:{a}');
+
+# Base Binaria
+a = int('10111', 2)
+print(f'a = 2 binaria:{a}');
+
+# base Octal
+a = int('12', 8)
+print(f'a = 8 Base octal:{a}');
+
+# base Hexadecimal
+a = int('A', 16)
+print(f'a = 10 Base Hexadecimal:{a}');
+
+# Base 5 sua valores son 0,1,2,3,4
+a = int('344', 5)
+print(f'a =5 base Base 5 :{a}');
