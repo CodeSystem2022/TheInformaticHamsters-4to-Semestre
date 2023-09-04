aa89a9b06ec4476b1bff0d764105d345d8869eba
Marcelo Alejandro Quispe <quispemarcelo@hotmail.com>
2023-09-04 08:03:19 -0300

    ASISTENCIA2JSMQUISPE

--- JAVASCRIPT/e-commerce2022/client/js/cart.js
+++ JAVASCRIPT/e-commerce2022/client/js/cart.js
@@ -1,12 +1,31 @@
-const modalContainer =document.getElementById("modal_container");
-const modalOverlay =document.getElementById("modal_overlay");
+const modalContainer = document.getElementById("modal-container");
+const modalOverlay = document.getElementById("modal-overlay");
+
+const cartBtn = document.getElementById("cart-btn");
 
 const displayCart = () => {
-    //modal Header
-    const modalHeader = document.createElement("div");
+    modalContainer.innerHTML = " ";
+    modalContainer.style.display = "block";
+    modalOverlay.style.display = "block";
+// modal Header 
+const modalHeader = document.createElement("div");
 
-    const modalClose = document.createElement("div");
-    modalClose.innerText = "❌";
-    modalClose.className = "modal-close";
-    modalHeader.append(modalClose)
+const modalClose = document.createElement("div");
+modalClose.innerText = "❌"
+modalClose.className = "modal-close";
+modalHeader.append(modalClose)
+
+modalClose.addEventListener("click", ()=>{
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+})
+
+const modalTitle = document.createElement("div");
+modalTitle.innerText = "Cart";
+modalTitle.className = "modal-title";
+modalHeader.append(modalTitle);
+
+modalContainer.append(modalHeader);
 };
+
+cartBtn.addEventListener("click", displayCart);
