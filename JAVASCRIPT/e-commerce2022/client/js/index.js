84b8036a4008eb770be0cd98d6872208f84f8f0f
jpnolan1979 <juan_pablo_nolan@hotmail.com>
2023-09-08 12:23:07 -0300

    se agrega codigo js semana4

--- JAVASCRIPT/e-commerce2022/client/js/index.js
+++ JAVASCRIPT/e-commerce2022/client/js/index.js
@@ -18,13 +18,22 @@ productos.forEach((product) => {
 
     // Creamos un evento de escucha atravez de un click
     buyButton.addEventListener("click", ()=>{
-        cart.push({ // agregamos al carrito 
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart)
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            cart.map((prod) => {
+                if(prod.id === product.id){
+                    prod.quanty++
+                }        
+            });
+        } else {
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+    
+            });
+        }
     })
 });
