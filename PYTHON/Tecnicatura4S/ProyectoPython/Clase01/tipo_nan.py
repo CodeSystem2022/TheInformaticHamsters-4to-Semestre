d8d700e2e25a9f63c49f54b5a679d0851e9b4204
jpnolan1979 <juan_pablo_nolan@hotmail.com>
2023-09-08 12:29:52 -0300

    se agrega codigo py semana4

---
+++ PYTHON/Tecnicatura4S/ProyectoPython/Clase01/tipo_nan.py
@@ -0,0 +1,15 @@
+import math
+from decimal import Decimal
+
+#NaN (Not a Namber)
+a = float("NaN")
+print(f'a: {a}')
+
+#Modulo math
+a = float("nan")
+print(f'Es de tipo NaN(Not a Namber)?: {math.isnan(a)}')
+
+#Modulo decimal
+a = Decimal("NaN")
+print(f'Es de tipo NaN(Not a Namber)?: {math.isnan(a)}')
+
