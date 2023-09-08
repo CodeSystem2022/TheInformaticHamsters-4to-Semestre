475a34324b0b3ff92bac3cae49bcae9681b99c50
Marcelo Alejandro Quispe <quispemarcelo@hotmail.com>
2023-09-08 10:20:12 -0300

    3_semana_3.4_MQUISPE

--- JAVASCRIPT/e-commerce2022/client/js/cart.js
+++ JAVASCRIPT/e-commerce2022/client/js/cart.js
@@ -11,9 +11,9 @@ const displayCart = () => {
 const modalHeader = document.createElement("div");
 
 const modalClose = document.createElement("div");
-modalClose.innerText = "❌"
-modalClose.className = "modal-close";
-modalHeader.append(modalClose)
+    modalClose.innerText = "❌"
+    modalClose.className = "modal-close";
+    modalHeader.append(modalClose)
 
 modalClose.addEventListener("click", ()=>{
     modalContainer.style.display = "none";
@@ -21,11 +21,37 @@ modalClose.addEventListener("click", ()=>{
 })
 
 const modalTitle = document.createElement("div");
-modalTitle.innerText = "Cart";
-modalTitle.className = "modal-title";
-modalHeader.append(modalTitle);
+    modalTitle.innerText = "Cart";
+    modalTitle.className = "modal-title";
+    modalHeader.append(modalTitle);
 
-modalContainer.append(modalHeader);
+    modalContainer.append(modalHeader);
 };
 
 cartBtn.addEventListener("click", displayCart);
+
+
+        const decrese = modalBody.querySelector(".quantity-btn-decrese");
+        decrese.addEventListener("click", () => { 
+            if(producto.quanty !== 1){
+                product.quanty--;
+                displayCart();
+            }
+        });    
+
+
+
+        const increse = modalBody.querySelector(".quantity-btn-increse");
+        increse.addEventListener("click", () => {
+            product.quanty++;
+            displayCart();
+        });
+
+
+    //delete
+
+        const deleteProduct = modalBody.querySelector(".delete-product");
+
+        deleteProduct.addEventListener("click", () => {
+            deleteCartProduct(product.id);
+        })
