f336291d0cfc7b452649cf5a9840509b3adc902d
miguelrs77 <miguelr.s77@gmail.com>
2023-09-09 19:52:00 -0300

    miAsitPythonMiguel

--- PYTHON/Tecnicatura4S/ProyectoPython/Clase01/tipo_str.py
+++ PYTHON/Tecnicatura4S/ProyectoPython/Clase01/tipo_str.py
@@ -8,5 +8,10 @@ mensaje = 'Hola ' 'Alumnos' + variable
 mensaje +=' , Terminamos'
 # print(mensaje)
 
+#  -> 1.4 Clase help() Alumno : Miguel Rodriguez Saquilan
+# Parte 1 -> Consulta con la clase str (String)
+# Parte 2 -> Consulta con el módulo math
+
+
 # Usamos la clase help para ayuda o documentacion (built-in)
 help(math.isnan)
