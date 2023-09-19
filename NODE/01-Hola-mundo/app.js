0021858c5f6879c60ef5e126a0db80307619b7a9
miguelrs77 <miguelr.s77@gmail.com>
2023-09-19 07:38:30 -0300

    Subiendo lo de Node

--- NODE/01-Hola-mundo/app.js
+++ NODE/01-Hola-mundo/app.js
@@ -2,13 +2,13 @@
 let nombre = 'Giuliana';
 console.log(nombre);
 
-nombre = 'Nadia';
+nombre = 'Totuss';
 console.log(nombre);
 
 const nombre1 = 'Giuliana'; // el valor de una constante no se puede cambiar
 console.log(nombre1);
 
-const nombre2 = 'Nadia';
+const nombre2 = 'Totuss';
 console.log(nombre2);
 
 console.log ('Hola Mundo desde Node');
