fd1a50cf791f32d1b6e7f0cf7dfb6f5054ab3bdd
Marcelo Alejandro Quispe <quispemarcelo@hotmail.com>
2023-09-22 11:59:35 -0300

    Proyecto-Tienda-Libros

---
+++ JAVA/Proyecto Tienda de Libros con SpringBoot/src/test/java/tienda_libros/TiendaLibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda_libros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
