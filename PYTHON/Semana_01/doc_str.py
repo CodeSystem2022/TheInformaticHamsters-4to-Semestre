58acf7ccb4160266fb1060fb09072712c98aa678
nadiaacosta <nadia.8acosta@gmail.com>
2023-09-25 13:34:38 +0200

    NadiaACosta-AsistenciaJSyPy-Semana06

---
+++ PYTHON/Semana_01/doc_str.py
@@ -0,0 +1,9 @@
+'''
+Podemos crear documentación
+
+'''
+
+"""
+
+Estas son las dos maneras de documentar
+"""
---
+++ PYTHON/Semana_01/mi_clase.py
@@ -0,0 +1,21 @@
+class MiClase:
+
+    """""""""
+    Este es un ejemplo de la documentación de esta clase.
+    Es solo para saber que aqui esta mi clase.
+    """""""""
+
+    def __init__(self):
+        ''''
+        Aquí esta el método dunder init
+        Este método es el de inicio para esta clase
+        ''''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mi que recibe parámetros
+        Detectó los parametros automaticamente
+        ;param param1: Este es el parametro número 1
+        ;param param2: Este es el parametro número 2
+        ;return: Este es el valor de retorno
+        '''
