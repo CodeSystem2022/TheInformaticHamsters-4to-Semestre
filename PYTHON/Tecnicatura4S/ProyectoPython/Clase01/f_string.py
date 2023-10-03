f9e3e383326a466759dd7794cebedb1d72ce41bc
Giuliana <giulygael2018@gmail.com>
2023-10-03 09:54:34 -0300

    Ingreso asistencia py Semana 07 - Giuliana Diaz

--- PYTHON/Tecnicatura4S/ProyectoPython/Clase01/f_string.py
+++ PYTHON/Tecnicatura4S/ProyectoPython/Clase01/f_string.py
@@ -4,4 +4,6 @@ sueldo = 3000
 mensaje = f'Nombre {nombre} Edad {edad} Sueldo {sueldo:.2f}'
 # print(mensaje)
 
-print(nombre, edad, sueldo, sep = ', ')
+# Alumno: Giuliana Paola Diaz Luna
+# 6.5 Uso del método print() para dar formato en la salida
+print(nombre, edad, sueldo, sep = ', ') # imprimimos la tupla con un separador que puede ser una coma, espacio
