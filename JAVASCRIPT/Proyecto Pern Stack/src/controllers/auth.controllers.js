645c376e0ca64d67b2a31affa02111b13552f827
jpnolan1979 <juan_pablo_nolan@hotmail.com>
2023-10-07 00:11:21 -0300

    agregamos codigo de js

--- JAVASCRIPT/Proyecto Pern Stack/src/controllers/auth.controllers.js
+++ JAVASCRIPT/Proyecto Pern Stack/src/controllers/auth.controllers.js
@@ -1,6 +1,19 @@
+import { pool } from "../db.js";
+
 export const signin = (req, res) => res.send("Ingresando");
 
-export const signup = (req, res) => res.send("Registrando");
+export const signup = async (req, res) => {
+    const {name, email, password} = req.body;
+    try {
+        const result =  await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, password]);
+        console.log(result);
+        return res.json(result.rows[0]);
+    } catch (error) {
+        if(error.code === "23505"){
+            return res.status(400).json({message: "El correo ya esta registrado"});
+        }    
+    }
+};
 
 export const signout = (req, res) => res.send("Cerrando sesion");
 
