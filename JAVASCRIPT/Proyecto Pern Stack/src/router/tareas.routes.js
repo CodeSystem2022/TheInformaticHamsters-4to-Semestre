bd24bcd8898cdb80d0eceab5d29c187551ef8344
nadiaacosta <nadia.8acosta@gmail.com>
2023-10-10 01:46:06 +0200

    Semana08-JS-Nadia

--- JAVASCRIPT/Proyecto Pern Stack/src/router/tareas.routes.js
+++ JAVASCRIPT/Proyecto Pern Stack/src/router/tareas.routes.js
@@ -1,17 +1,18 @@
 import { Router} from "express";
 import { actualizarTarea, crearTarea, eliminarTarea, listarTarea, listarTareas } from "../controllers/tareas.controllers.js";
+import { isAuth } from "../../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.get('/tareas', listarTareas );
+router.get('/tareas',isAuth, listarTareas );
 
-router.get('/tareas/:id', listarTarea);
+router.get('/tareas/:id',isAuth, listarTarea);
 
-router.post('/tareas', crearTarea );
+router.post('/tareas',isAuth, crearTarea );
 
-router.put('/tareas/:id', actualizarTarea);
+router.put('/tareas/:id',isAuth, actualizarTarea);
 
-router.delete('/tareas/:id', eliminarTarea);
+router.delete('/tareas/:id',isAuth, eliminarTarea);
 
 
 
