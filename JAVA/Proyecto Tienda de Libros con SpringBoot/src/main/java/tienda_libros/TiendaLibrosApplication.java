60091953792cbf9def862e6eed05475ae99b6526
jpnolan1979 <juan_pablo_nolan@hotmail.com>
2023-10-09 00:41:19 -0300

    Agregamos codigo de java

--- JAVA/Proyecto Tienda de Libros con SpringBoot/src/main/java/tienda_libros/TiendaLibrosApplication.java
+++ JAVA/Proyecto Tienda de Libros con SpringBoot/src/main/java/tienda_libros/TiendaLibrosApplication.java
@@ -2,12 +2,24 @@ package tienda_libros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.tienda_libros.vista.LibroFrom;
+import java.awt.EventQueue;
 
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+	
+		ConfigurableApplicationContext contextoSpring =
+					new SpringApplicationBuilder(TiendaLibrosApplication.class)
+							.headless(false)
+							.web(WebApplicationType.NONE)
+							.run(args);
 
+			//Ejecutamos el codigo para cargar el formulario
+			EventQueue.invokeLater(() -> { //Metodo Lambda
+				//Obtenemos el objeto form a traves del Spring
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			});
+	}
 }
