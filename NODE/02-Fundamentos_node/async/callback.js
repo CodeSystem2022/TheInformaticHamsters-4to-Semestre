dac6ac6015487959abe521c4bf222c6f830efdf9
jpnolan1979 <juan_pablo_nolan@hotmail.com>
2023-10-12 10:09:56 -0300

    se agrega codigo de node S8

---
+++ NODE/02-Fundamentos_node/async/callback.js
@@ -0,0 +1,22 @@
+
+function hola(nombre, miCallback){
+    setTimeout(function() {
+        console.log("Hola " + nombre);
+        miCallback(nombre);
+    }, 1000);
+   
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log("Adios " + nombre);
+        otroCallback();
+    }, 1000);
+}
+
+console.log("Iniciando el proceso...");
+hola("Carlos", function(nombre) {
+    adios(nombre, function() {
+        console.log("Termiando el proceso...");
+    });
+});
