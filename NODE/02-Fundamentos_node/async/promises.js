01a1ef56089cb8b9b81461d532bcfdaa8082a0f9
miguelrs77 <miguelr.s77@gmail.com>
2023-10-29 21:58:21 -0300

    archivo promises

---
+++ NODE/02-Fundamentos_node/async/promises.js
@@ -0,0 +1,44 @@
+function hola (nombre){
+    return  new Promise(function(resolve, reject){
+        setTimeout( function() {
+            console.log('Hola '+ nombre);
+            resolve(nombre);
+            }, 1000);      
+    });
+}
+
+
+function hablar(nombre){
+    return new Promise((resolve,reject)=>{ // Usamos la sintaxis ES6
+        setTimeout(function () {
+            console.log('bla bla , bla  ');
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+ function adios(nombre){
+    return new Promise((resolve, reject)=>{
+        setTimeout( function(){
+            console.log('Adios '+ nombre)
+           // resolve();
+           reject('Hay un error')
+        },1000);
+    });
+}
+
+// llamamos a la funcion
+console.log('Iniciando el proceso.....');
+hola('Miguelon')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminando el proceso');
+    })
+    .catch(error => {
+        console.log('Ha habido un error: ');
+        console.log(error);
+});
+
